ff84e1485d53ec2a6b7b241816a097f9a092161c
angadxsingh <angadnahal@gmail.com>
2024-07-13 15:41:53 +0530

    commit

---
+++ charsize.cpp
@@ -0,0 +1,14 @@
+// c++ program to demonstrate correct size of various data types on a computer
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Size of char:" << sizeof(char) << endl;
+    cout << "Size of int:" << sizeof(int) << endl;
+    cout << "Size of float:" << sizeof(float) << endl;
+    cout << "Size of double:" << sizeof(double) << endl;
+    cout << "Size of long:" << sizeof(long) << endl;
+
+    return 0;
+}
